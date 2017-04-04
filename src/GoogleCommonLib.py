8531cc3735d80953cc69b4cc369ffe398e4d1c55
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-04-04 16:51:20 +0100

    more static libs

--- src/GoogleCommonLib.py
+++ src/GoogleCommonLib.py
@@ -4,6 +4,65 @@ class GoogleCommonLib:
     """Common methods for handling Google Drive requests"""
 
     @staticmethod
+    def getShareableLink(drive, gfile):
+        """Pulls existing, or generates a new one"""
+        url = GoogleCommonLib.getExistingShareableLink( drive, gfile )
+        if url == None:
+            print("%s : making url shareable" % gfile, file=cerr)
+            url = GoogleCommonLib.generateShareableLink( drive, gfile )
+
+        return url
+
+
+    @staticmethod
+    def getExistingShareableLink(drive, file):
+        if file['shared']:
+            url = file['alternateLink'].strip()
+            if len(url) > 10:
+                return url
+        return None
+
+    
+    @staticmethod    
+    def generateShareableLink(drive, file):
+        perm = { 'type': 'anyone',
+                 'value' : 'anyone',
+                 'role' : 'reader',
+                 'withLink': True }
+
+        file.InsertPermission( perm );
+        return file['alternateLink']
+
+
+    @staticmethod
+    def listFilesInFolder(drive, folderID):
+        filelist=[]
+        file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % folderID}).GetList()
+        for f in file_list:
+            if f['mimeType']!='application/vnd.google-apps.folder': # if not folder
+                filelist.append(f)
+
+        if len(filelist) == 0:
+            print(parent, "has no files within", file=cerr)
+            exit(-1)
+
+        return filelist
+
+
+    @staticmethod
+    def uploadFile(drive, parentID, filename, title = None):
+        """UploadsFile, returns ID"""
+        if title == None:
+            title = filename
+
+        f = drive.CreateFile({"title" : title,
+                              "parents": [{"kind": "drive#fileLink", "id": parentID}]})
+        f.SetContentFile( filename )
+        f.Upload()
+        return f
+        
+
+    @staticmethod
     def createFolder(drive, foldername):
         """Returns new folder ID on completion"""
         newfold = drive.CreateFile({'title': foldername, 
