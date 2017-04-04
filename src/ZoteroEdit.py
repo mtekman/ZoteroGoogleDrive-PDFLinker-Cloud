327048c054b6813cefab4ffcdf26de71040a3621
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-04-04 16:54:00 +0100

    debugging md5s

--- src/ZoteroEdit.py
+++ src/ZoteroEdit.py
@@ -3,6 +3,14 @@
 from pyzotero import zotero
 from sys import stderr as cerr
 
+def zoteroDebug(apikey,libid, collid):
+    zot = zotero.Zotero( libid, "user", apikey )
+    items = zot.collection_items(collid)
+    import pdb
+    pdb.set_trace()
+
+
+
 
 class ZoteroDispatch:
 
@@ -319,3 +327,13 @@ class ZoteroEdit:
     def printItem(self,item):
         print(item)
         return 1
+
+
+
+### Example of attachment using zotero storage:
+# {'key': 'PSZA7XPI', 'version': 14, 'library': {'type': 'user', 'id': 3808237, 'name': 'mtekman', 'links': {'alternate': {'href': 'https://www.zotero.org/mtekman', 'type': 'text/html'}}}, 'links': {'self': {'href': 'https://api.zotero.org/users/3808237/items/PSZA7XPI', 'type': 'application/json'}, 'alternate': {'href': 'https://www.zotero.org/mtekman/items/PSZA7XPI', 'type': 'text/html'}, 'up': {'href': 'https://api.zotero.org/users/3808237/items/JQZPDSGU', 'type': 'application/json'}, 'enclosure': {'type': 'application/pdf', 'href': 'https://api.zotero.org/users/3808237/items/PSZA7XPI/file/view', 'title': 'allegro.pdf', 'length': 90942}}, 'meta': {}, 'data': {'key': 'PSZA7XPI', 'version': 14, 'parentItem': 'JQZPDSGU', 'itemType': 'attachment', 'linkMode': 'imported_file', 'title': 'Gudbjartsson et al_2005_Allegro version 2.pdf', 'accessDate': '', 'url': '', 'note': '', 'contentType': 'application/pdf', 'charset': '', 'filename': 'Gudbjartsson et al_2005_Allegro version 2.pdf', 'md5': '11e622b16099f9a64a562e9e2a3d94af', 'mtime': 1470313393000, 'tags': [], 'relations': {}, 'dateAdded': '2016-08-04T12:23:13Z', 'dateModified': '2017-03-17T13:34:14Z'}}
+
+### Example of same attachment using external storage:
+# {'key': 'LARDKYJA', 'version': 2793, 'parentItem': '2FWFTHJN', 'itemType': 'attachment', 'linkMode': 'linked_file', 'title': '2011_Cliodynamics_Baker_demographic-str-theory-Rome.pdf', 'accessDate': '', 'url': '', 'note': '', 'contentType': 'application/pdf', 'charset': '', 'path': 'W:\\hstanesc\\info\\articles\\2011_Cliodynamics_Baker_demographic-str-theory-Rome.pdf', 'tags': [], 'relations': {}, 'dateAdded': '2017-03-03T10:01:13Z', 'dateModified': '2017-03-03T10:01:13Z'}
+
+# Problem is that external storage does NOT have md5s to work with, and must use title
